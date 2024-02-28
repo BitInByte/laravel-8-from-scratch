4d43b639a2ca1de5f4f9255686c217357d9919ef
bitinbyte <bitinbyte@outlook.com>
2024-02-28 16:12:06 +0100

    Auth finish

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Validation\Rule;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        // create the user
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            // 'username' => 'required|min:3|max:255|unique:users,username',
+            'username' => [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::unique('users', 'username')
+            ],
+            'email' => 'required|email|max:255|unique:users,email',
+            // 'password' => 'required|min:7|max:255'
+            'password' => [
+                'required',
+                'min:7',
+                'max:255'
+            ]
+        ]);
+
+        // $attributes['password'] = bcrypt($attributes['password']);
+
+        $user = User::create($attributes);
+
+        // log user in
+        auth()->login($user);
+
+        // session()->flash('success', 'Your account has been created.');
+
+        return redirect('/')->with('success', 'Your account has been created.');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,13 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'username',
+    // ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -42,6 +44,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // public function getUsernameAttribute($username)
+    // {
+    //     return ucwords($username); // $person->username
+    // }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 
     public function posts()
     {
